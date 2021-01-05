356efaf4fd3c2ea6e76b7cfc23102de8e018b7f2
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-01-05 12:23:12 +0900

    configの設定を変更

--- scripts/addVersion.py
+++ scripts/addVersion.py
@@ -39,38 +39,38 @@ def main():
     api_key = shotgun_config["api_key"]
     target_dir = shotgun_config["target_dir"]
     project_id = shotgun_config["project_id"]
-    link_asset_id = shotgun_config["link_asset"]
-    link_task_id = shotgun_config["link_task"]
     user_id = shotgun_config["user"]
 
-    shotgun_up = config["create"]
+    shotgun_up = config["assetVersion00"]
     code = shotgun_up["code"]
     description = shotgun_up["description"]
     status = shotgun_up["sg_status_list"]
+    link_asset_id = shotgun_up["link_asset"]
+    link_task_id = shotgun_up["link_task"]
 
     sg = Shotgun(url, script_name=script_name, api_key=api_key)
+
+    image = []
 
     target_list = os.listdir(target_dir)
     for file in target_list:
         if ".png" in file:
-            images.append(file)
+            image.append(file)
 
-    version = sg.create("Version", {
-        "project": { "type": "Project", "name": "testProject" , "id": project_id , "include_archived_projects": False},
-        "entity": {"type": "Asset", "id": link_asset_id },
-        "sg_task": {"type": "Task", 'id': link_task_id },
-        "user": {"type": "HumanUser", 'id': user_id},
-        "code": code,
-        "description" : description,
-        "sg_status_list": status,
-        "sg_path_to_frames": file[0],
-    })
-    
-    sg.upload("Version", version["id"], os.path.join(target_dir, file[0]), field_name = "sg_uploaded_movie", display_name = file[0])
-    sg.update("Version", version["id"], {"sg_status_list": status}, multi_entity_update_modes={"assets": "add"})
+        version = sg.create("Version", {
+            "project": { "type": "Project", "name": "testProject" , "id": project_id , "include_archived_projects": False},
+            "entity": {"type": "Asset", "id": link_asset_id },
+            "sg_task": {"type": "Task", 'id': link_task_id },
+            "user": {"type": "HumanUser", 'id': user_id},
+            "code": code,
+            "description" : description,
+            "sg_status_list": status,
+            "sg_path_to_frames": image,
+        })
 
-    test = sg.activity_stream_read("Version", version["id"])
-    print(test)
+        #sg.upload("Version", version["id"], os.path.join(target_dir, image), field_name = "sg_uploaded_movie", display_name = image)
+        #sg.update("Version", version["id"], {"sg_status_list": status}, multi_entity_update_modes={"assets": "add"})
+
     return 0
 
 if __name__ == "__main__":
